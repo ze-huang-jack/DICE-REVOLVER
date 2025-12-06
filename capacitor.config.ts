27d454caf92429ec60b2db75597436d85ef1b514
ze huang(jack) <suqiuliang727@gmail.com>
2025-12-06 14:14:56 +0800

    fix(build): change appId to ensure multiple APKs can coexist on device

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.jack.dice-revolver',
   appName: 'dice-revolver',
   webDir: 'dist'
 };
